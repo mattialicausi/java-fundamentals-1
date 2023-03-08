284e25886bc2a474161e8462016e54a84dbd6817
mattialicausi <licausivitomattia@gmail.com>
2023-03-08 14:15:14 +0100

    aggiunto variabili per il calcolo del prezzo del biglietto, aggiunto scanner per avere l'età dell'utente

--- src/org/lessons/java/TrainTicket.java
+++ src/org/lessons/java/TrainTicket.java
@@ -1,7 +1,25 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class TrainTicket {
     public static void main(String[] args) {
+
+        //variabili per l'utente
+        int userAge;
+
+        //variabili fisse
+        double kmPrice = 0.21;
+        int underageDiscount = 20;
+        int adultDiscount = 40;
+
+        //uso lo scanner per chiedere l'età all'utente e lo inserisco in userAge
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Inserisci la tua età");
+        userAge = Integer.parseInt(scan.nextLine());
+
+        System.out.println("La tua età è: " + userAge + " anni");
+
 
     }
 }

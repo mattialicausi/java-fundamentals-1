353bf6212a086682c9f0734d0ca3b28c8550a134
mattialicausi <licausivitomattia@gmail.com>
2023-03-08 14:23:07 +0100

    aggiunto scanKm per sapere quanti km vuole percorrere l'utente

--- src/org/lessons/java/TrainTicket.java
+++ src/org/lessons/java/TrainTicket.java
@@ -7,6 +7,8 @@ public class TrainTicket {
 
         //variabili per l'utente
         int userAge;
+        int kmUser;
+        double finalPrice;
 
         //variabili fisse
         double kmPrice = 0.21;
@@ -14,11 +16,21 @@ public class TrainTicket {
         int adultDiscount = 40;
 
         //uso lo scanner per chiedere l'età all'utente e lo inserisco in userAge
-        Scanner scan = new Scanner(System.in);
+        Scanner scanAge = new Scanner(System.in);
         System.out.println("Inserisci la tua età");
-        userAge = Integer.parseInt(scan.nextLine());
+        userAge = Integer.parseInt(scanAge.nextLine());
 
+        //uso lo scanner per chiedere quanti km vuole percorrere l'utente e li inserisco in kmUser
+        Scanner scanKm = new Scanner(System.in);
+        System.out.println("Quanti km vuoi percorrere? ");
+        kmUser = Integer.parseInt(scanKm.nextLine());
+
+        //stampo in terminale età e km da percorrere
         System.out.println("La tua età è: " + userAge + " anni");
+        System.out.println("Vuoi percorrere: " + kmUser + " km");
+
+        //calcolo il prezzo finale in base all'età dell'utente
+
 
 
     }
